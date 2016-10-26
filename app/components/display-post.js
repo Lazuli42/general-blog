501b3ce5002ba10f5579d2ab7c46402d1691b1f9
kyle fisher and zachary baum <zachary@>
2016-10-26 15:47:40 -0400

    added update functionality, cosmic latte coloration.

--- app/components/display-post.js
+++ app/components/display-post.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isImageShowing: false,
+  updatePostForm: false,
   actions: {
+    update(post, params) {
+      this.sendAction('update', post, params);
+    },
 
     delete(post){
       if (confirm('is you for positive?')) {
